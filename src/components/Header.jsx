2127b1e4a89de5bc814918815c3d15475b6e124a
Sandy Caba <joriel@sandys-air.mynetworksettings.com>
2023-01-23 21:38:02 -0500

    completed header, created file structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { GiRobinHoodHat } from 'react-icons/gi'
+import { RxMagnifyingGlass } from 'react-icons/rx'
+import "./Header.css"
+
+export default function Header() {
+  return (
+    <div className='header-wrapper'>
+      {/* logo */}
+      <div className=' header-logo'>
+      <GiRobinHoodHat className='logo'/>
+      </div>
+      {/* search */}
+      <div className='header-search'>
+        <div className='header-search-container'>
+          <input type="text" placeholder='Search...' className='search-box' />
+        </div>
+      </div>
+      {/* menu items */}
+      <div className='header-menu-items'>
+        <a href='#' className='stock-item'>Free Stocks</a>
+        <a href='#'>Portfolio</a>
+        <a href='#'>Cash</a>
+        <a href='#'>Messages</a>
+        <a href='#'>Account</a>
+      </div>
+    </div>
+  )
+}
+
---
+++ src/components/Newsfeed.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Newsfeed.css"
+
+export default function Newsfeed() {
+  return (
+    <div className=' newsfeed'>
+    <div className=' newsfeed-container'>
+
+      </div>
+      </div>
+  )
+}
+
